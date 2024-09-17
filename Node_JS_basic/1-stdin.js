9c1a6c5d5580aa559888785394f40c8315a97ad0
OscarRapale <you@example.com>
2024-09-17 12:28:20 -0400

    Task 1 added

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -8,7 +8,7 @@ const rl = readline.createInterface({
 console.log('Welcome to Holberton School, what is your name?');
 
 rl.on('line', (input) => {
-  process.stdout.write(`Your name is: ${input}\r`);
+  process.stdout.write(`Your name is: ${input}\n`);
   rl.close();
 });
 
